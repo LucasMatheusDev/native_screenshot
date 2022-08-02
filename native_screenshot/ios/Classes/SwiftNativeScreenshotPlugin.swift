8612f92cdc153d16ec282c6def6613d111f3a363
Lucas Matheus <81427844+LucasMatheusDev@users.noreply.github.com>
2022-08-02 14:24:53 -0300

    Add files via upload

---
+++ native_screenshot/ios/Classes/SwiftNativeScreenshotPlugin.swift
@@ -0,0 +1,147 @@
+import Flutter
+import UIKit
+
+public class SwiftNativeScreenshotPlugin: NSObject, FlutterPlugin {
+    var controller :FlutterViewController!
+    var messenger :FlutterBinaryMessenger
+    var result :FlutterResult!
+    var screenshotPath :String!
+
+    init(controller: FlutterViewController, messenger: FlutterBinaryMessenger) {
+        self.controller = controller
+        self.messenger = messenger
+
+        super.init()
+    } // init()
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "native_screenshot", binaryMessenger: registrar.messenger())
+
+        let app = UIApplication.shared
+        let controller :FlutterViewController = app.delegate!.window!!.rootViewController as! FlutterViewController
+
+        let instance = SwiftNativeScreenshotPlugin(
+            controller: controller,
+            messenger: registrar.messenger()
+        ) // let instance
+
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    } // register()
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "takeScreenshot" {
+            handleTakeScreenshot(result: result)
+        } else if call.method == "takeScreenshotImage" {
+            handleTakeScreenshotImage(result: result)
+        }
+        result(FlutterMethodNotImplemented)
+
+        return
+    }
+
+    func handleTakeScreenshot(result: @escaping FlutterResult) {
+
+        self.result = result
+
+        // FIX: add posibility to choose if gallery or some path
+        takeScreenshot(view: controller.view)
+    } // handleTakeScreenshot()
+
+    func getScreenshotName() -> String {
+        let format = DateFormatter()
+        format.dateFormat = "yyyymmddHHmmss"
+        
+        let fname :String = "native_screenshot-\(format.string(from: Date())).png"
+        
+        return fname
+    } // getScreenshotName()
+    
+    func getScreenshotPath() -> URL? {
+        let paths :[URL] = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        guard let dir = paths.first else {
+            return nil
+        } // guard
+        
+        return dir.appendingPathComponent( getScreenshotName() )
+    } // getScreenshotPath()
+    
+    func writeImageToDefaultPath(image: UIImage) -> String? {
+        guard let imageData = image.pngData() else {
+            return nil
+        } // guard
+        
+        guard let path = getScreenshotPath() else {
+            return nil
+        } // guard
+        
+        guard let _ = try? imageData.write(to: path) else {
+            return nil
+        } // guard
+        
+        return path.path
+    } // writeImageToDefaultPath()
+    
+    @objc
+    func savedToGalleryDone(image: UIImage, error: NSError?, contextInfo: UnsafeMutableRawPointer?) {
+        if error == nil && self.screenshotPath != nil && !self.screenshotPath.isEmpty {
+            result(self.screenshotPath)
+        } else {
+            result(nil)
+        }
+    } // savedToGalleryDone()
+    
+    func writeImageToGallery(image :UIImage) {
+        UIImageWriteToSavedPhotosAlbum(
+            image,
+            self,
+            #selector(savedToGalleryDone),
+            nil
+        ); // UIImageWriteToSavedPhotosAlbum()
+    } // writeImageToGallery()
+    
+    func captureImage(view: UIView) -> UIImage? {
+        let scale :CGFloat = UIScreen.main.scale
+
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.isOpaque, scale)
+
+        view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        let optionalImage :UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return optionalImage
+    }
+
+    func takeScreenshot(view: UIView, toImageGallery :Bool = true) {
+        let optionalImage :UIImage? = captureImage(view: view)
+        
+        guard let image = optionalImage else {
+            result(nil)
+            
+            return
+        } // guard no image
+        
+        guard let path = writeImageToDefaultPath(image: image) else {
+            result(nil)
+            
+            return
+        } // guard cannot write image
+        
+        self.screenshotPath = path
+ 
+        writeImageToGallery(image: image)
+    } // takeScreenshot()
+
+    func handleTakeScreenshotImage(result: @escaping FlutterResult) {
+        let optionalImage :UIImage? = captureImage(view: controller.view)
+        guard let image = optionalImage else {
+            result(nil)
+            return
+        }
+        guard let imageData = image.pngData() else {
+            result(nil)
+            return
+        }
+        result(imageData)
+    } // handleTakeScreenshotImage()
+
+} // SwiftNativeScreenshotPlugin
---
+++ native_screenshot/lib/native_screenshot.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+/// Class to capture screenshots with native code working on background
+class NativeScreenshot {
+  /// Comunication property to talk to the native background code.
+  static const MethodChannel _channel =
+      const MethodChannel('native_screenshot');
+
+  /// Captures everything as is shown in user's device.
+  ///
+  /// Returns [null] if an error ocurrs.
+  /// Returns a [String] with the path of the screenshot.
+  static Future<String?> takeScreenshot() async {
+    final String? path = await _channel.invokeMethod('takeScreenshot');
+
+    return path;
+  } // takeScreenshot()
+
+	/// Captures everything as is shown in user's device.
+	///
+	/// Returns a [List<int>] with the png data for the screenshot,
+	/// or [null] if an error occurs.
+  static Future<List<int>?> takeScreenshotImage() async {
+    final List<int>? image = await _channel.invokeMethod('takeScreenshotImage');
+    return image;
+  }
+} // NativeScreenshot
